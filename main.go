28321e7518f76617029bfe83d108af3c743c8bc0
Retasusan <kenta9736@gmail.com>
2026-01-14 03:14:15 +0900

    implement /healthz

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok\n"))
+	})
+	addr := getenv("LISTEN_ADDR", ":8080")
+	fmt.Printf("listening on %s\n", addr)
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+func getenv(k, def string) string {
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+	return def
 }
