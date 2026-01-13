599925973ee9382ca58439f118c1fee2f954d1b4
Retasusan <kenta9736@gmail.com>
2026-01-14 03:38:02 +0900

    add comment

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// health check
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok\n"))
 	})
+
 	addr := getenv("LISTEN_ADDR", ":8080")
 	fmt.Printf("listening on %s\n", addr)
 	srv := &http.Server{
